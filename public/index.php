86d120a6542fbac13674adb6369a4a6dac3c98e1
mochoa <mochoa@protonmail.com>
2025-11-27 15:10:59 -0500

    added more content. replaced background image with video

--- public/index.php
+++ public/index.php
@@ -1,33 +1,18 @@
 <?php
 //$html = file_get_contents('test.html');
 $html = file_get_contents(__DIR__.'/../templates/header.html');
+
+$html .= '<!-- Background Video -->
+
+  <video id="bg-video" autoplay muted loop playsinline>
+    <source src="img/2K_Tech_Globe_green-slow.mp4" type="video/mp4">
+  </video>
+<div id="video-overlay"></div>';
+
 $html .= file_get_contents(__DIR__.'/../templates/logo-div.html');
 $html .= file_get_contents(__DIR__.'/../templates/menu-div.html');
 $html .= file_get_contents(__DIR__.'/../templates/hero-div.html');
-$html .= file_get_contents(__DIR__.'/../templates/body-01-div.html');
-
-/*
-$html .= '<h1>Welcome to the JS8Call-Improved Website</h1><hr>';
-
-$html .= '<div style="width: 840px;"><img src="img/js8call-2025-11-16.png" width="800" alt="JS8Call Improved Screenshot">
-';
-
-$html .= '<p>Download Version 2.4.0 <a href="https://github.com/JS8Call-improved/JS8Call-improved">Click Here</a></p>';
-$html .= '<p>Comments, questions or suggestions? Email js8call-improved @ proton.me';
-$html .= '<p></p><b>NOTE:</b> This website is currently under development. More content will be published soon.<br>
-<h2>Comming Soon</h2>
-<ul>
-<li>Additional Resources</li>
-<li>Blog Posts</li>
-<li>Community Forum</li>
-<li>Training Videos</li>
-<li>Updated Documentation</li>
-<li>And Much More...</li>
-</ul>';
-$html .= '</div>';
-
-
-*/
+$html .= file_get_contents(__DIR__ . '/../templates/features-div.html');
 $html .= file_get_contents(__DIR__.'/../templates/footer.html');
 
 echo $html;
