161bbd52e6c2102abd09dd15eb01de57d89086d3
mochoa <mochoa@protonmail.com>
2025-12-04 07:48:16 -0500

    Added 1 more video

--- public/training.php
+++ public/training.php
@@ -10,22 +10,31 @@ $html .= '<div class="features-outer-box">
     
 
     <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=XZPfBNgn-P0"><img src="img/js8call_vid1_jjs_2025-12.png" width="400"></a>
+        <a href="https://www.youtube.com/watch?v=XZPfBNgn-P0" target="_blank" rel="noopener noreferrer"><img src="img/js8call_vid1_jjs_2025-12.png" class="shadow-video"></a>
         <!--<iframe src="https://www.youtube.com/embed/XZPfBNgn-P0" title="JS8Call Vid 1 | Introduction, Install, Configure, First Tour" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
         <div>
             <h2 class="video-text-18">JS8Call Vid 1 | Introduction, Install, Configure, First Tour</h2>
             <p class="video-text-44">Mama Kissy shares how the X39 patch transformed her health, boosting her energy, alleviating chronic pain, and enhancing her overall well-being. She highlights its natural, non-invasive approach and encourages others to try it for improved vitality and quality of life..</p>
-            <p class="video-text-18"><b>Note:</b> This is a 10 minute video.<span class="right-align">Download X39 product brochure: <a href="media/X39_product_brochure.pdf" target="_blank">X39 Product Brochure</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p>
+            <p class="video-text-18"><strong>Note:</strong> This is a 38 minute video.</p>
         </div>
     </div>
     <hr>
     <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=XZPfBNgn-P0"><img src="img/js8call_vid2_jjs_2025-12.png"></a>
+        <a href="https://www.youtube.com/watch?v=koi3hA--YMw" target="_blank" rel="noopener noreferrer"><img src="img/js8call_vid2_jjs_2025-12.png"></a>
         <!--<iframe src="https://www.youtube.com/embed/XZPfBNgn-P0" title="JS8Call Vid 1 | Introduction, Install, Configure, First Tour" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
         <div>
             <h2 class="video-text-18">JS8Call Vid 2 | Operation & Features</h2>
             <p class="video-text-44">Mama Kissy shares how the X39 patch transformed her health, boosting her energy, alleviating chronic pain, and enhancing her overall well-being. She highlights its natural, non-invasive approach and encourages others to try it for improved vitality and quality of life..</p>
-            <p class="video-text-18"><b>Note:</b> This is a 10 minute video.<span class="right-align">Download X39 product brochure: <a href="media/X39_product_brochure.pdf" target="_blank">X39 Product Brochure</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p>
+            <p class="video-text-18"><strong>Note:</strong> This is a 27 minute video.</p>
+        </div>
+    </div>
+    <hr>
+    <div class="video-box">   
+        <iframe width="560" height="315" class="shadow-video" src="https://www.youtube.com/embed/eE84BgTNSVA" title="Next-Level JS8CALL Features &amp; Updates" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div>
+            <h2 class="video-text-18">Next-Level JS8CALL Features & Updates</h2>
+            <p class="video-text-44">Mama Kissy shares how the X39 patch transformed her health, boosting her energy, alleviating chronic pain, and enhancing her overall well-being. She highlights its natural, non-invasive approach and encourages others to try it for improved vitality and quality of life..</p>
+            <p class="video-text-18"><strong>Note:</strong> This is a 1 hour, and 30 minute video.</p>
         </div>
     </div>
     <hr>
