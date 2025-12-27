4cbba713cc5676d69c53b179705f9ffe16841207
Chris-AC9KH <chriskristin.olson@icloud.com>
2025-12-27 14:23:33 -0600

    Improve linux build scripts to build JS8Call from master KDE Plasma does not have ~/.local/applications or icons directory by default

--- tools/INSTALL_JS8Call-improved_Fedora.sh
+++ tools/INSTALL_JS8Call-improved_Fedora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# (C) copyright 2025 Chris Olson AC9KH, Joseph Counsil K0OG. This script is for building and installing JS8Call-improved from source code.
+# (C) copyright 2025 Chris Olson AC9KH, Joseph Counsil K0OG. This script is for building and installing JS8Call from source code.
 
 # variables
 red="\033[0;31m"
@@ -26,7 +26,7 @@ if [ ! "$(id -u)" -ne 0 ]; then
 You MUST be logged in as a normal user to run this script.
 
 It is recommended to add your username to the sudo group. We need to install
-system libraries that are required to build JS8Call-improved. This must be done as a
+system libraries that are required to build JS8Call. This must be done as a
 sudo user.
 
 If you have not done this, this script will NOT RUN AS ROOT!${clear}"
@@ -41,21 +41,21 @@ user_dialog
 echo "Checking for existing installation....."
 if [ -e ${HOME}/.local/bin/JS8* ]; then
   user_dialog
-  echo "We have found an existing JS8Call-improved installation. Do you want
+  echo "We have found an existing JS8Call installation. Do you want
   to uninstall it? If you select No(n) the newest version will be built and
   over-write the old one."
-  read -p "Uninstall current JS8Call-improved? Yes(y) / No(n):-" UNINSTALL
+  read -p "Uninstall current JS8Call? Yes(y) / No(n):-" UNINSTALL
   if [ ${UNINSTALL} = "y" ]; then
-    echo "removing JS8Call-improved binary...."
+    echo "removing JS8Call binary...."
     rm ~/.local/bin/JS8*
     echo "removing Qt 6.9.3....."
     rm -rf ~/.local/lib/Qt
-    echo "removing JS8Call-improved.desktop......"
+    echo "removing JS8Call.desktop......"
     rm ~/.local/share/applications/JS8*.desktop
-    echo "removing JS8Call-improved icon......"
+    echo "removing JS8Call icon......"
     rm ~/.local/share/icons/icon_128.svg
     echo "
-    JS8Call-improved is uninstalled
+    JS8Call is uninstalled
     If you wish to re-install run this script again."
     user_dialog
     exit 1
@@ -65,8 +65,8 @@ fi
 clear
 user_dialog
 echo -e "This script will fetch necessary sources and dependencies to build
-JS8Call-improved and install it for the local user only on your system.
-If you already have an existing JS8Call-improved installation from your distribution
+JS8Call and install it for the local user only on your system.
+If you already have an existing JS8Call installation from your distribution
 or downloaded from the Releases it will not affect that and you will be able
 run either version of the program, but not at the same time. Please see the 
 NOTES on the next page:"
@@ -75,19 +75,19 @@ user_dialog
 read -p "Press Enter to continue" </dev/tty
 clear
 echo "NOTES:
-The newest versions of JS8Call-improved require Qt v6.9.3 to run correctly. Most
-linux distributions do not package Qt6.9.3. The JS8Call-improved project has pre-compiled
+The newest versions of JS8Call require Qt v6.9.3 to run correctly. Most
+linux distributions do not package Qt6.9.3. The JS8Call project has pre-compiled
 Qt6.9.3 libraries by Chris-AC9KH that this script will fetch and install. This
 will not affect whatever version of Qt you have installed from your distribution.
-The two versions can co-exist and JS8Call-improved will be linked with the
+The two versions can co-exist and JS8Call will be linked with the
 Qt6.9.3 installation, which will be in your ~/.local/lib/Qt directory.
 
 If you run this script a second time after installation, it will ask if you want
-to uninstall JS8Call-improved. The installation of Qt6.9.3 will also be removed
+to uninstall JS8Call. The installation of Qt6.9.3 will also be removed
 during uninstall. But during build the Qt6.9.3 library archive that will be downloaded
 will be saved in your Downloads folder. If you don't wish to keep it, delete it.
 
-The JS8Call-improved menu item is tested with Gnome. It should
+The JS8Call menu item is tested with Gnome. It should
 work on KDE since linux desktops are supposed to follow the conventions laid out
 by freedesktop.org but this seems to be not always the case. If you don't get a
 menu item on KDE et al under the Other category, the binary is installed to ~/.local/bin
@@ -103,11 +103,11 @@ echo "AUDIO:
 Newer versions of Qt use FFmpeg audio backend. ALSA audio is deprecated. If you
 are running an older version of JS8Call(2.2) it might be using ALSA backend. There will
 be no audio on a Linux system with JS8Call-2.3 and later unless it is using PipeWire or
-PulseAudio. When you start up JS8Call-improved it will use your current JS8Call settings
+PulseAudio. When you start up JS8Call it will use your current JS8Call settings
 if you have an existing installation. There is some incompatibility with audio and text
 encoding between JS8Call 2.2 (Fortran/Qt5) and 2.3 and newer (C++/Qt6).
 
-The current JS8Call-improved codebase is coded for Qt6.9.3, developed on MacOS. It attempts
+The current JS8Call codebase is coded for Qt6.9.3, developed on MacOS. It attempts
 to be compatible with linux but we rely on linux users to provide feedback on issues that
 may arise to identify linux-specific problems. This is our primary purpose in offering this
 relatively easy method to install and run the latest code, with the proper Qt library, on linux.
@@ -179,8 +179,8 @@ fi
 sleep 3
 clear
 
-# fetch JS8Call-improved source code from JS8Call-improved
-echo "fetching JS8Call-improved source code....."
+# fetch JS8Call source code from JS8Call-improved
+echo "fetching JS8Call source code....."
 user_dialog
 if [ ! -d ~/development/JS8Call-improved ]; then
   git clone "https://github.com/JS8Call-improved/JS8Call-improved.git"
@@ -198,7 +198,7 @@ else
 fi
 sleep 3
 
-# prepare to build JS8Call-improved
+# prepare to build JS8Call
 if [ ! -d ~/.local/lib/Qt ]; then
   echo "extracting Qt6 to ~/.local/lib....."
   user_dialog
@@ -221,7 +221,7 @@ clear
 cd JS8Call-improved
 BRANCH=$(git branch --show-current 2>&1)
 user_dialog
-echo "JS8Call-improved Build Details:
+echo "JS8Call Build Details:
 Qt version: 6.9.3 with FFmpeg audio. Requires PulseAudio or PipeWire
 Branch: JS8Call-improved ${BRANCH}"
 user_dialog
@@ -240,6 +240,14 @@ cmake --build .
 if [ ! -d ~/.local/bin ]; then
   mkdir ~/.local/bin
 fi
+
+# KDE Plasma does not have icons and applications directory by default
+# so check to see if these exist and if not, create them
+if [ ! -d ~/.local/share/applications ]; then
+  mkdir ~/.local/share/applications
+  mkdir ~/.local/share/icons
+fi
+
 cp JS8Call ~/.local/bin
 cp ../artwork/icon_128.svg ~/.local/share/icons/
 touch ~/.local/share/applications/JS8Call.desktop
@@ -254,19 +262,21 @@ echo "Terminal=false" >> ~/.local/share/applications/JS8Call.desktop
 clear
 user_dialog
 echo "DONE!"
-echo "Do you want to remove the JS8Call-improved development directory?
+echo "Do you want to remove the JS8Call development directory?
 Don't worry the program will run fine without it and you can always
 re-fetch it with this script later."
 user_dialog
-read -p "remove JS8Call-improved source tree? Yes(y) / No(n):-" CLEANUP
+read -p "remove JS8Call source tree? Yes(y) / No(n):-" CLEANUP
 if [ "${CLEANUP}" = "y" ]; then
   rm -rf ~/development
-  echo "JS8Call-improved source tree removed.
+  echo "JS8Call source tree removed.
+  JS8Call launcher icon should be in your application launcher menu.
   install script has exited......."
   user_dialog
 else
-  echo "The development directory with the JS8Call-improved source tree has been \
-  left on your system located at ~/development"
+  echo "The development directory with the JS8Call source tree has been \
+  left on your system located at ~/development \
+  JS8Call launcher icon should be in your application launcher menu."
   user_dialog
   error
 fi
